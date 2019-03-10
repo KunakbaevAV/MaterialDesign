0711cafc0718a41685425f799ff90bc0d92b317a
Artem <kunakbaevav@yandex.ru>
2019-03-10 16:32:01 +0500

    Реализовал смену стилей, но не работает.

--- app/src/main/java/com/example/materialdesign/ThemesFragment.java
+++ app/src/main/java/com/example/materialdesign/ThemesFragment.java
@@ -1,8 +1,10 @@
 package com.example.materialdesign;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
@@ -13,6 +15,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.Objects;
 
@@ -40,5 +44,26 @@ public class ThemesFragment extends ListFragment {
                 android.R.layout.simple_list_item_1,
                 themes);
         setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(@NonNull ListView l, @NonNull View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        if (position == 0) {
+            changeTheme(R.style.ThemeRed, "красная");
+        } else if (position == 1) {
+            changeTheme(R.style.ThemeBlue, "синяя");
+        } else if (position == 2) {
+            changeTheme(R.style.ThemeYellow, "желтая");
+        }
+    }
+
+    private void changeTheme(int id, String nameTheme) {
+        Activity activity = getActivity();
+        assert activity != null;
+        activity.setContentView(R.layout.activity_main);
+        activity.setTheme(id); //Почему не меняется тема?
+        activity.recreate();
+        Toast.makeText(activity, "Используется " + nameTheme + " тема", Toast.LENGTH_SHORT).show();
     }
 }
