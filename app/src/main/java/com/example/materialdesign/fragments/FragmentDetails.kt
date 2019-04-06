46dcd7065092fbc64c5d9677b1f6d095fe46604b
Artem <kunakbaevav@yandex.ru>
2019-04-06 17:20:06 +0500

    Хотел сделать анимацию при нажатии на монетку, но не пойму, что не так с этим TransitionSet?

--- app/src/main/java/com/example/materialdesign/fragments/FragmentDetails.kt
+++ app/src/main/java/com/example/materialdesign/fragments/FragmentDetails.kt
@@ -6,16 +6,23 @@ import android.os.Bundle
 import android.text.SpannableString
 import android.text.Spanned
 import android.text.style.BackgroundColorSpan
+import android.transition.ArcMotion
+import android.transition.ChangeBounds
+import android.transition.TransitionPropagation
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.OvershootInterpolator
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.transition.CircularPropagation
+import androidx.transition.Fade
+import androidx.transition.TransitionSet
 import com.example.materialdesign.R
 import com.example.materialdesign.model.Card
 
-class FragmentDetails() : Fragment() {
+class FragmentDetails : Fragment() {
     lateinit var card: Card
     lateinit var title: TextView
     lateinit var description: TextView
@@ -29,8 +36,6 @@ class FragmentDetails() : Fragment() {
         val view = inflater.inflate(R.layout.fragment_details, container, false)
         initUI(view)
         updateUI()
-//        useSpannebleString()
-
         return view
     }
 
@@ -54,6 +59,28 @@ class FragmentDetails() : Fragment() {
         description = view.findViewById(R.id.card_description)
         image = view.findViewById(R.id.card_image)
         icon_type = view.findViewById(R.id.icon_type)
+        icon_type.setOnClickListener { showParamsFragment() }
         icon_number = view.findViewById(R.id.icon_number)
+    }
+
+    private fun showParamsFragment() {
+        if (fragmentManager != null) {
+            val fragment = FragmentParams()
+
+            val changeBounds = ChangeBounds()
+            changeBounds.pathMotion = ArcMotion()
+            changeBounds.duration = 500
+            changeBounds.interpolator = OvershootInterpolator(1.5f)
+
+            val set = TransitionSet()
+            set.addTransition(Fade())
+//            set.addTransition(changeBounds) // На этом моменте я застрял и не понимаю как сделать анимацию
+
+            fragment.sharedElementEnterTransition = changeBounds
+
+            fragmentManager!!.beginTransaction()
+                    .replace(R.id.container_details, fragment)
+                    .addToBackStack("details").commit()
+        }
     }
 }
